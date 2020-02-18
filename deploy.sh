3b2e5edb882d38b75622ad3a13192cc7481c7939
Kumar Rahul <ec2-user@ip-172-31-39-98.ap-south-1.compute.internal>
2020-02-18 09:17:29 +0000

    added khejkffjejehjef

--- deploy.sh
+++ deploy.sh
@@ -2,3 +2,8 @@ hello
 with this script we are going to deploy the hello world java file
 
 hello we have now added some more features
+in
+
+writing code to test git clean
+
+ 04 added script for the user to create the bulk users
